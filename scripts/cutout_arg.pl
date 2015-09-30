12f44916ad85938b21b9cd92e3903bd3a36c2de1
Lab User <labuser@ec2732air.nwls.earlham.edu>
2015-09-30 14:45:34 -0400

    "histogram of arginine

--- scripts/cutout_arg.pl
+++ scripts/cutout_arg.pl
@@ -7,12 +7,22 @@ my @pdbs = glob("pdbs/*pdb");
 
 my $bldr = HackaMol->new;
 
+my %bin;
+
 foreach my $pdb (@pdbs){
   say $pdb;
+  my @atoms = $bldr->read_file_atoms($pdb);
   my @cz_arg = grep{ $_->name    eq 'CZ' }
-               grep{ $_->resname eq 'ARG'} $bldr->read_file_atoms($pdb);
+               grep{ $_->resname eq 'ARG'} @atoms;
   #HackaMol::Molecule->new(atoms=>[@cz_arg])->print_pdb($pdb.".shit.pdb");
-  HackaMol::Molecule->new(atoms=>[@cz_arg])->print_pdb;
+ # HackaMol::Molecule->new(atoms=>[@cz_arg])->print_pdb;
+foreach my $cz (@cz_arg){
+	my @cluster = grep {$cz->distance($_) <= 5} @atoms;
+#	HackaMol::Molecule->new(atoms=>[@cluster])->print_pdb;
+	$bin{$_}++ foreach ( map{ $_->resname} @cluster );
 }
+}
+
+print Dump \%bin;
 
 
